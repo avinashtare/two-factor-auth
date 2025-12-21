6b9c8c743e0a4eaf112cd8df920e6595756bff34
Avinash Tare <avinashtare5@gmail.com>
2025-12-22 00:03:25 +0530

    fix zod validation error

--- src/helper/error.helper.ts
+++ src/helper/error.helper.ts
@@ -1,8 +1,11 @@
 export class ApplicationException extends Error {
   public statusCode: number;
-  constructor(message: string, statusCode: number) {
+  public data: any;
+
+  constructor(message: string, statusCode: number, data?: any) {
     super(message);
     this.statusCode = statusCode;
+    this.data = data;
     this.name = this.constructor.name;
   }
 }
