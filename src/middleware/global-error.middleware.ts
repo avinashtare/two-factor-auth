bfdca71c55f0bc1015036c250489e0c2547395ee
Avinash Tare <avinashtare5@gmail.com>
2025-12-22 23:30:24 +0530

    fix: jwt error response

--- src/middleware/global-error.middleware.ts
+++ src/middleware/global-error.middleware.ts
@@ -2,15 +2,11 @@ import { ErrorRequestHandler } from "express";
 import envConfig from "../config/env.config";
 import { ApplicationException } from "../helper/error.helper";
 import { ZodError } from "zod";
+import { JsonWebTokenError } from "jsonwebtoken";
 
 type TGlobalError = Error | ApplicationException;
 
-const globalErrorMiddleware: ErrorRequestHandler = (
-  err: TGlobalError,
-  _req,
-  res,
-  _next
-) => {
+const globalErrorMiddleware: ErrorRequestHandler = (err: TGlobalError, _req, res, _next) => {
   let statusCode = 500;
   let errorMessage = err.message;
   let data = null;
@@ -25,28 +21,33 @@ const globalErrorMiddleware: ErrorRequestHandler = (
     const issues = err.issues;
 
     if (issues.length > 0 && issues[0]?.message) {
-      data = issues.map(f => ({ message: f.message, field: f.path[0] }));
+      data = issues.map((f) => ({ message: f.message, field: f.path[0] }));
 
       statusCode = 422;
       errorMessage = `Invalid input`;
     }
+  }
+  // jwt erro handle
+  else if (err instanceof JsonWebTokenError) {
+    statusCode = 401;
+    errorMessage = "Invalid Token";
   }
 
   const response = {
     success: false,
     message: errorMessage,
     statusCode,
-    data
+    data,
   };
 
   // logging
   const errorLog = {
     ...response,
     stack: err.stack,
-    environment: envConfig.NODE_ENV
+    environment: envConfig.NODE_ENV,
   };
 
-  // console.dir(errorLog, { depth: null, colors: true });
+  console.dir(errorLog, { depth: null, colors: true });
 
   return res.status(statusCode).json(response);
 };
